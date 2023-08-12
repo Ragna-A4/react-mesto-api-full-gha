ce668aad16832e66eae4f47ac8a552e1c401c6f4
Александра Чернова <av_chernova@MacBook-Pro-Aleksandra.local>
2023-08-12 14:57:42 +0300

    fix: fix regex

--- backend/utils/regular.js
+++ backend/utils/regular.js
@@ -1,1 +1,1 @@
-module.exports.validateUrl = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{2,}\.[a-zA-Z0-9()]{2,}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/m;
+module.exports.validateUrl = /^https?:\/\/?([\w-]{1,32}\.)?[-a-zA-Z0-9@:%._+~#=]{2,}\.[a-zA-Z0-9()]{2,}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/m;
